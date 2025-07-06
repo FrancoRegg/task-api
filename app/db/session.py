476693c2789a242cdadd6c020a3706222138691b
FrancoRegg <francoreggiardo97@gmail.com>
2025-07-06 12:16:14 +0200

    Inicio postgres

--- app/db/session.py
+++ app/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://usuario:password@localhost:5432/task_db"
+DATABASE_URL = "postgresql://postgres:JazminRocco43201@localhost:5432/task_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
