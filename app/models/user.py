a0c5ff07c54df40a11d05217d49c0603b98b2ad5
FrancoRegg <francoreggiardo97@gmail.com>
2025-07-06 12:17:08 +0200

    Actualizacion

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
+from app.models import Base
 
 
 class User(Base):
