e6cecc9e173ec17efc44877c85629a3b21fcf9c4
FrancoRegg <francoreggiardo97@gmail.com>
2025-07-06 12:13:25 +0200

    Ruta de autenticacion

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api.v1.endpoints.health import router as health_router
+from app.api.v1.endpoints.auth import router as auth_router
 
 app = FastAPI(title="Task API")
 
 app.include_router(health_router, prefix="/api/v1")
+app.include_router(auth_router, prefix="/api/v1")
