a0147406221ba3c71460ccc7fccdd9c72b4ce18d
FrancoRegg <francoreggiardo97@gmail.com>
2025-07-09 19:16:15 +0200

    Ajustes variables

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,20 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:JazminRocco43201@localhost:5432/task_db"
-    JWT_SECRET: str = "supersecreto"
-    ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    environment: str = "development"
 
-    class Config:
-        env_file = ".env"
+    postgres_server: str
+    postgres_port: int
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    database_url: str
+
+    secret_key: str
+    access_token_expire_minutes: int = 30
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
 settings = Settings()
