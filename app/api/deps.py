ebede29e91fe59fcd22c69aafa3f600e00303fa3
FrancoRegg <francoreggiardo97@gmail.com>
2025-07-10 19:15:01 +0200

    Validacion de credenciales

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,6 +1,12 @@
 from typing import Generator
-from app.db.session import SessionLocal
 from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+
+from app.crud.user import get_user_by_email
+from app.utils.security import verify_access_token
+
+from app.db.session import SessionLocal
 
 
 def get_db() -> Generator[Session, None, None]:
@@ -9,3 +15,26 @@ def get_db() -> Generator[Session, None, None]:
         yield db
     finally:
         db.close()
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/login")
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="No se pudo validar credenciales",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        token_data = verify_access_token(token)
+    except Exception as exc:
+        raise credentials_exception from exc
+
+    user = get_user_by_email(db, token_data.email)
+    if user is None:
+        raise credentials_exception
+    return user
